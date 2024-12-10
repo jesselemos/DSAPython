7b44870addb22b4cf3d588ed4e4c1552f6ce8b3d
thiago_acam <thiago.acam@gmail.com>
2024-12-10 22:15:56 +0000

    adding

---
+++ Thiago/Studies/Program.cs
@@ -0,0 +1,14 @@
+﻿using Studies.Benchmarks;
+using BenchmarkDotNet.Running;
+
+namespace Studies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<MergeSortBenchmark>();
+        }
+
+    }
+}
