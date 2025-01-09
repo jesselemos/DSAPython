1356ec8cc379237033f4e0dcdaad1cc18c58ead8
Jesse Lemos <jesselemos@gmail.com>
2025-01-09 20:12:03 +0000

    Bubble Sort Advanced By TCO and JLS
    The best Bubble Sort you ever see in your life.

---
+++ Thiago/Studies/BubbleSort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Studies
+{
+    /*
+     BubbleSort (Bolha) its idea is get the biggest array number and bring it to the lastest position of the array
+     */
+    internal class BubbleSort
+    {
+
+        public int[] SortArray(int[] toSort)
+        {
+            for (int j = 0; j < toSort.Length - 1; j++)
+            {
+                for (int i = 0; i < toSort.Length - j - 1; i++)
+                {
+                    if (toSort[i] > toSort[i + 1])
+                    {
+                        var aux = toSort[i];
+                        toSort[i] = toSort[i + 1];
+                        toSort[i + 1] = aux;
+                    }
+                }
+            }
+
+            return toSort;
+        }
+
+        /*5 4 3
+          4 3 5
+
+         */
+    }
+}
--- Thiago/Studies/Program.cs
+++ Thiago/Studies/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Studies.Benchmarks;
 using BenchmarkDotNet.Running;
 using System;
+using static JetBrains.FormatRipper.FileExplorer.FileTypeExplorer;
 
 namespace Studies
 {
@@ -10,14 +11,19 @@ namespace Studies
         {
             //BenchmarkRunner.Run<MergeSortBenchmark>();
 
-            int[] arr = new int[] { 1, 2, 3, 4, 5 };
-            var search = new BinarySearch();
-            
-            for(var value = 0; value < 7; value++)
-            {
-                var result = search.SearchRec(arr, value, 0, arr.Length);
-                Console.WriteLine($"value= {value} index = {result}");
-            }
+            int[] arr = new int[] { 5,4,3,2,1 };
+
+            var bubble = new BubbleSort();
+            var ret = bubble.SortArray(arr);
+            Console.WriteLine($"{ret}");
+
+            //var search = new BinarySearch();
+
+            //for(var value = 0; value < 7; value++)
+            //{
+            //    var result = search.SearchRec(arr, value, 0, arr.Length);
+            //    Console.WriteLine($"value= {value} index = {result}");
+            //}
 
         }
 
